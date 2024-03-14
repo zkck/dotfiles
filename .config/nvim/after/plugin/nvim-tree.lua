337d7c5a18bca16afb7891217f08ad552df7b730
Zak Cook <zakcook4@gmail.com>
2024-03-14 16:46:41 +0100

    Nvim tree always show focused file

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,4 +1,8 @@
-require("nvim-tree").setup()
-local api = require("nvim-tree.api")
+require("nvim-tree").setup {
+    update_focused_file = {
+        enable = true,
+    }
+}
 
+local api = require("nvim-tree.api")
 vim.keymap.set({ 'n' }, '<leader>e', api.tree.open, {})
