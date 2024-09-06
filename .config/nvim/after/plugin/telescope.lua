8184b39417250b8a4501f8aced2b844e1f481693
Zak Cook <zakcook4@gmail.com>
2024-09-07 00:11:41 +0200

    Add shortcut to search for all symbols

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -14,3 +14,4 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>fS', builtin.lsp_dynamic_workspace_symbols, {})
