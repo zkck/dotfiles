f6815205082db46472339c48f37b503175846cf9
Zak Cook <zak.cook@ipt.ch>
2024-10-15 07:48:56 +0200

    Rust setup with overlay in nix flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,66 +1,73 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: 
-  let 
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in
-  {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      packages = with pkgs; [
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
 
-        # core stack
-        tmux
-        git
-        fish
-        neovim
-        podman
+            # core stack
+            tmux
+            git
+            fish
+            neovim
+            podman
 
-        # for editing neovim config
-        lua-language-server
+            # for editing neovim config
+            lua-language-server
 
-        # k8s
-        kubectl
-        k9s
+            # k8s
+            kubectl
+            k9s
 
-        # shell utilities
-        zoxide
-        lazygit
-        fzf
-        fd
-        ripgrep
-        less
+            # shell utilities
+            zoxide
+            lazygit
+            fzf
+            fd
+            ripgrep
+            less
 
-        # editing
-        dprint
-        tree-sitter
+            # editing
+            dprint
+            tree-sitter
 
-        # golang
-        go
-        gopls
+            # rust
+            (rust-bin.stable.latest.default.override {
+                extensions = ["rust-src" "rust-analyzer"];
+            })
 
-        # rust
-        cargo
-        rustfmt
-        rust-analyzer
+            # golang
+            go
+            gopls
 
-        # python
-        python3
-        pyright
-      ];
+            # python
+            python3
+            pyright
+          ];
 
-      shellHook = "tmux";
+          shellHook = "tmux";
 
-      EDITOR = "nvim";
-      PAGER = "less";
+          EDITOR = "nvim";
+          PAGER = "less";
 
-      # fix for cargo https://nixos.wiki/wiki/Development_environment_with_nix-shell#Troubleshooting
-      NIX_ENFORCE_PURITY = "0";
+          # fix for cargo https://nixos.wiki/wiki/Development_environment_with_nix-shell#Troubleshooting
+          NIX_ENFORCE_PURITY = "0";
 
-      # fix for git https://github.com/NixOS/nixpkgs/issues/58132
-      GIT_SSH = "/usr/bin/ssh";
-    };
-  };
+          # fix for git https://github.com/NixOS/nixpkgs/issues/58132
+          GIT_SSH = "/usr/bin/ssh";
+        };
+      }
+      );
 }
